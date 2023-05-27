1c77cb3055f0f92e8b2e772106cab4461dad4445
Gilbert <gveitia13@gmail.com>
2023-05-27 04:30:44 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+//Components
+import {AppComponent} from './app.component';
+import {BuscarImagenComponent} from './components/buscar-imagen/buscar-imagen.component';
+import {ListarImagenComponent} from './components/listar-imagen/listar-imagen.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {SpinnerComponent} from './shared/spinner/spinner.component';
+import {ErrorComponent} from './shared/error/error.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarImagenComponent,
+    ListarImagenComponent,
+    NavbarComponent,
+    SpinnerComponent,
+    ErrorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
